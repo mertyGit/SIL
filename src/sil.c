4061ca5649c5b2791189f8df2aeb1ad8eb621b4b
Remco Schellekens <merty@xs4all.nl>
2022-09-04 16:15:43 +0200

    even more basic example now

--- src/sil.c
+++ src/sil.c
@@ -36,38 +36,34 @@
 --- Code
 #include <stdio.h>
 #include "sil.h"
-#include "log.h"
 
 int main() {
-  SILLYR *drawing;
+  SILLYR *whitelayer,*greenlayer;
 
-  // Initialize SIL; Create a window of 200x200 with title "basic example"
-  sil_initSIL(200,200,"basic example",NULL);
+  // Initialize SIL; Create a window of 200x200 with title "basic"
+  sil_initSIL(200,200,"basic",NULL);
+  
+  // Create a layer in this window at position 10,10 and dimensions 60x60
+  whitelayer=sil_addLayer(10,10,60,60,SILTYPE_ABGR);
 
-  // Create layer in this window at position 10,10 and dimension 150x150
-  drawing=sil_addLayer(10,10,150,150,SILTYPE_ABGR);
+  // paint layer white, no transparancy 
+  sil_paintLayer(whitelayer,SILCOLOR_WHITE,255);
 
-  // paint background of layer white, no transparancy
-  sil_paintLayer(drawing,SILCOLOR_WHITE,255);
-  // set foreground color - in this case the border - to dark green, no transparancy
-  sil_setForegroundColor(SILCOLOR_DARK_GREEN,255);
-  // set background color - in this fill of rectangle - to green, opacity at 50
-  sil_setBackgroundColor(SILCOLOR_GREEN,50);
-  // set width of lines (for border) to 4
-  sil_setDrawWidth(4);
-  // Draw a rectangle inside layer "drawing" at 15,15 and dimension 100x100
-  sil_drawRectangle(drawing,15,15,100,100);
+  // Create another layer in this window at position 40,40 and dimensions 100x100
+  greenlayer=sil_addLayer(40,40,100,100,SILTYPE_ABGR);
+
+  // paint layer green, 50% transparancy  
+  sil_paintLayer(greenlayer,SILCOLOR_GREEN,127);
 
   // update display (window in this case)
   sil_updateDisplay();
 
-  // Wait till event makes it quit, like alt+F4 or closing window
+  // Wait untill an event makes it quit, like alt+F4 or closing window
   sil_mainLoop();
 
   // Cleanup
   sil_destroySIL();
 }
-  
 ---
 
  */
